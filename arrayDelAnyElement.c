5b0196f1b69bcf540ad1b25d2528bc005060bd53
Shan Shuvo <rtsshuvo6434@gmail.com>
2023-01-21 16:41:06 +0600

    Array Learning

--- arrayDelAnyElement.c
+++ arrayDelAnyElement.c
@@ -3,7 +3,7 @@
 void main()
 {
     int arr[N] = {110,120,130,140,150};
-    int count = 0 ,i;
+    int count = 0 ,i, element, position;
     while(arr[i] != '\0')
     {
         count++;
@@ -17,12 +17,34 @@ void main()
     {
         printf("%5d", arr[i]);
     }
-    int element;
-    printf("Enter the element you wish to delete: ");
+
+    printf("\nEnter the element you wish to delete: ");
     scanf("%d", &element);
+
     for(i=0; i<count; i++)
     {
         if(arr[i] == element)
-        break;
+        {
+            position = i;
+        }
+    }
+    printf("\nThe index is : %d\n", position);
+
+    if(position > count)
+    {
+        printf("\nDeletion is NOT POSIBBLE\n");
+    }
+    else
+    {
+      for (i = position; i < count; i++)
+      {
+        arr[i] = arr[i+1];
+      }
+      printf("\nResultant array:\n");
+
+      for (i = 0; i < count-1; i++)
+      {
+        printf("%d\n", arr[i]);
+      }
     }
 }
